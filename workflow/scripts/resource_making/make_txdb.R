2fdd87dec58b80d7ed23f36a066ebf9cc9828e87
mal2017 <>
2023-02-21 16:56:42 -0500

    include seqlevel info in txdb

--- workflow/scripts/resource_making/make_txdb.R
+++ workflow/scripts/resource_making/make_txdb.R
@@ -1,10 +1,14 @@
 library(AnnotationDbi)
 library(GenomicFeatures)
 library(tidyverse)
+library(BSgenome.Dmelanogaster.UCSC.dm6)
+
+# flybase == NCBI
+genome <- BSgenome.Dmelanogaster.UCSC.dm6
+seqlevelsStyle(genome) <- "NCBI"
 
 txdb <- ifelse(exists("snakemake"),snakemake@input[["gtf"]],
                "resources/dmel-all-r6.41.gtf.gz") %>%
-  makeTxDbFromGFF()
-
+  makeTxDbFromGFF(chrominfo = seqinfo(genome))
 
 saveDb(txdb, file = snakemake@output[["txdb"]])
